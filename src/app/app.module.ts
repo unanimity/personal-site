072a05869241dafbe09b1a2ae8e8427f1981697d
sha <unanimity@list.ru>
2018-09-28 16:54:22 +0300

    jj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatSidenavModule} from '@angular/material';
+import { ScrollToModule } from 'ng2-scroll-to-el';
+const appRoutes: Routes = [
+/*  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },*/
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Sergei Kharlamov' }
+  }/*,
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }/*,
+  { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  RouterModule.forRoot(appRoutes, { enableTracing: true } ),
+    BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatButtonModule, MatCheckboxModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
