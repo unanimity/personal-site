ad234dc26a737f2f664994aa39dd3da757cd0cc4
sha <unanimity@list.ru>
2018-09-30 14:10:12 +0300

    logo stop event

--- src/app/fragments/logo/logo.component.ts
+++ src/app/fragments/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 
 
@@ -9,18 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoComponent implements OnInit {
 public blink = false;
+private scrolable = false;
   constructor() {
-
 
 
   }
 
   ngOnInit() {
     this.blink = true;
-    setInterval(() => { this.blink = !this.blink; }, 1000);
+
+    setInterval(() => { if (this.scrolable) { this.blink = !this.blink; } else { this.blink = true; } }, 1000);
 
   }
 
+
+
+  @HostListener('window:scroll', ['$event'])
+  doSomething(event) {
+   //  console.log('Scroll Event', window.pageYOffset);
+    this.scrolable = (window.pageYOffset) > 500;
+  }
 
 
 }
