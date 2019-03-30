f1178ac91cadad43a069f72ad3847113a01bb496
Sergei Kharlamov <unanimity@list.ru>
2019-03-30 13:47:53 +0300

    add json db

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ posts: [], user: {}, count: 0 })
+  .write();
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
